2a85c105093eafccb803b09d1296f79dd8265d4f
kdkavanagh <kdkavanagh@gmail.com>
2024-05-06 04:16:55 -0500

    Add option to prevent collapsing sidebar/groups (#132)
    * version bump
    
    * Adding missing package
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Add option to prevent collapsing sidebar/groups
    
    * Formatting only
    
    * PR feedback
    
    ---------
    
    Co-authored-by: Shannon <mail@shannonhochkins.com>

--- packages/components/src/Group/index.tsx
+++ packages/components/src/Group/index.tsx
@@ -4,8 +4,9 @@ import { Row, Column, fallback, CardBase, CardBaseProps, mq, type AvailableQueri
 import { motion, AnimatePresence } from "framer-motion";
 import { ErrorBoundary } from "react-error-boundary";
 
-const StyledGroup = styled(CardBase)<{
+const StyledGroup = styled(CardBase) <{
   collapsed: boolean;
+  collapsible: boolean;
 }>`
   background-color: var(--ha-S200);
   color: var(--ha-S200-contrast);
@@ -23,21 +24,22 @@ const StyledGroup = styled(CardBase)<{
       display: flex;
       align-items: center;
       transition: padding var(--ha-transition-duration) var(--ha-easing);
-      &:before {
-        content: "${({ collapsed }) => (collapsed ? "+" : "-")}";
+      ${({ collapsible, collapsed }) =>
+        collapsible &&
+        `&:before {
+        content: "${collapsed ? "+" : "-"}";
         display: inline-block;
         color: var(--ha-A400);
         width: 1rem;
-      }
-    }
+      }`}
   }
   ${({ collapsed }) => `
     ${mq(
-      ["xxs", "xs"],
-      `
+    ["xxs", "xs"],
+    `
       padding: ${collapsed ? "0 1rem" : "0 1rem 1rem"};
     `,
-    )}
+  )}
   `};
 `;
 
@@ -83,6 +85,8 @@ export interface GroupProps extends Omit<CardBaseProps, OmitProperties> {
   gap?: React.CSSProperties["gap"];
   /** should the group be collapsed by default @default false */
   collapsed?: boolean;
+  /** Whether the group can be collapsed by the end-user @default true */
+  collapsible?: boolean;
   /** fired when the group header section is clicked */
   onClick?: (event: React.MouseEvent<HTMLElement, MouseEvent>) => void;
 }
@@ -95,7 +99,8 @@ function _Group({
   alignItems = "center",
   layout = "row",
   collapsed = false,
-  className,
+  collapsible = true,
+  className, 
   onClick,
   ...rest
 }: GroupProps): React.ReactNode {
@@ -113,11 +118,14 @@ function _Group({
       disableRipples
       className={`${className ?? ""} group`}
       collapsed={_collapsed}
+      collapsible={collapsible}
       {...rest}
     >
       <Header
         onClick={(event) => {
-          setCollapsed(!_collapsed);
+          if (collapsible) {
+            setCollapsed(!_collapsed);
+          }
           if (onClick) onClick(event);
         }}
         className="header-title"
@@ -127,7 +135,7 @@ function _Group({
       </Header>
 
       <AnimatePresence initial={false}>
-        {!_collapsed && (
+        {(!collapsible || !_collapsed) && (
           <motion.section
             className="content"
             style={{
