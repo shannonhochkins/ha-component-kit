dcbf785a813d4ba883b6763925d7e220aadc90d9
Shannon Hochkins <mail@shannonhochkins.com>
2025-02-24 12:38:36 +1100

    Updating locales and types (#216)

--- packages/core/src/hooks/useLocale/locales/index.ts
+++ packages/core/src/hooks/useLocale/locales/index.ts
@@ -1,338 +1,338 @@
 // This file is dynamically generated
 type LocaleData = Record<LocaleKeys, string>;
-import { type Locales, LocaleKeys } from "./types";
+import { type Locales, LocaleKeys } from './types';
 const CACHE: Partial<Record<Locales, LocaleData>> = {};
 const locales = [
   {
-    code: "af",
-    hash: "35d045184f58f561b4bc7ba4d2d911da",
-    name: "Afrikaans",
+    "code": "af",
+    "hash": "8630f5ad63754a78bb4e6d474d126afb",
+    "name": "Afrikaans"
   },
   {
-    code: "ar",
-    hash: "0d2e95d56dcdca1e6a144ce105817791",
-    name: "العربية",
+    "code": "ar",
+    "hash": "6f2a5cc65f95b4ae73dc56caae6f6517",
+    "name": "العربية"
   },
   {
-    code: "bg",
-    hash: "b17b3127c1b69ff592899b4c7d96bbd7",
-    name: "Български",
+    "code": "bg",
+    "hash": "f8f1df563b665b678b743ef925772f50",
+    "name": "Български"
   },
   {
-    code: "bn",
-    hash: "869b5bca70e3cebee1c88fd895c96b30",
-    name: "বাংলা",
+    "code": "bn",
+    "hash": "c9ffeaa26b81a847ca6068802be7c56d",
+    "name": "বাংলা"
   },
   {
-    code: "bs",
-    hash: "3fbc6dee83c50b4b4786209bbd7061e7",
-    name: "Bosanski",
+    "code": "bs",
+    "hash": "ac0eeb59706c911685a1b1458bcc5996",
+    "name": "Bosanski"
   },
   {
-    code: "ca",
-    hash: "b1795bffeaf87ba7e0e64a2ddb745de5",
-    name: "Català",
+    "code": "ca",
+    "hash": "bbbec74c0ccecaf095162d8de45d2e97",
+    "name": "Català"
   },
   {
-    code: "cs",
-    hash: "febbf246033b7cbe0150c2fb1d414bf7",
-    name: "Čeština",
+    "code": "cs",
+    "hash": "c188a5cb8fb43d5d9e13583ce44e572b",
+    "name": "Čeština"
   },
   {
-    code: "cy",
-    hash: "0c6acf7d9f17481e84cce5e20f02ec7d",
-    name: "Cymraeg",
+    "code": "cy",
+    "hash": "05eb4f989eaf3dc2ce33c9d619be1d6c",
+    "name": "Cymraeg"
   },
   {
-    code: "da",
-    hash: "c19577728c002292f175db46027e7373",
-    name: "Dansk",
+    "code": "da",
+    "hash": "339ce5fdee7e58c405110f779bc5ff8f",
+    "name": "Dansk"
   },
   {
-    code: "de",
-    hash: "cba7defc692673637a96b70570ceef07",
-    name: "Deutsch",
+    "code": "de",
+    "hash": "2d1d439503c3a08d486e5d204d8f1adb",
+    "name": "Deutsch"
   },
   {
-    code: "el",
-    hash: "bc8caa565b76f08deca4264f9a29dc08",
-    name: "Ελληνικά",
+    "code": "el",
+    "hash": "b38d52af8ceb6bfc1d07990ec46a79e1",
+    "name": "Ελληνικά"
   },
   {
-    code: "en",
-    hash: "94909bac5e64e02325a35ce1e634366a",
-    name: "English",
+    "code": "en",
+    "hash": "5729b7572ff47ce9f30086ff0c4dd0f9",
+    "name": "English"
   },
   {
-    code: "en-GB",
-    hash: "6af3174fd3548aca91f541ea908460fc",
-    name: "English (GB)",
+    "code": "en-GB",
+    "hash": "838cb396158e78ef9332a8458b6ed096",
+    "name": "English (GB)"
   },
   {
-    code: "eo",
-    hash: "3246119d8f2bca52d771e8f97529a036",
-    name: "Esperanto",
+    "code": "eo",
+    "hash": "610b8cc283fdaaac147120f662f1c1e5",
+    "name": "Esperanto"
   },
   {
-    code: "es",
-    hash: "d820e789d1110327648ab9764aa9fede",
-    name: "Español",
+    "code": "es",
+    "hash": "4e7d16e6b594d73904ab7a6109a9c4fd",
+    "name": "Español"
   },
   {
-    code: "es-419",
-    hash: "70c7b855d563d2d7ebd4ef146a692aee",
-    name: "Español (Latin America)",
+    "code": "es-419",
+    "hash": "c646aef8a3eee1b84b4e2d330854b966",
+    "name": "Español (Latin America)"
   },
   {
-    code: "et",
-    hash: "44d23ebe19629ec5d90f67be5f68462d",
-    name: "Eesti",
+    "code": "et",
+    "hash": "c069b8049566f1d9abc4fbc6e6409fa1",
+    "name": "Eesti"
   },
   {
-    code: "eu",
-    hash: "ea5cee7c13f5bbc7a657bbc70d89a858",
-    name: "Euskara",
+    "code": "eu",
+    "hash": "bd66828fc8bfc355fb454afffa10ac08",
+    "name": "Euskara"
   },
   {
-    code: "fa",
-    hash: "637cc7d104d822daa899aa42a04ab5d2",
-    name: "فارسی",
+    "code": "fa",
+    "hash": "c2dbc2172e7ae8a210738c3fb4608b84",
+    "name": "فارسی"
   },
   {
-    code: "fi",
-    hash: "47bd175ebc4cab5c76a65a4695005265",
-    name: "Suomi",
+    "code": "fi",
+    "hash": "13b1eb9c1eb2844e17e3c9c29099a891",
+    "name": "Suomi"
   },
   {
-    code: "fy",
-    hash: "ba251e18dc216f9f0e62a2aede0ea718",
-    name: "Frysk",
+    "code": "fy",
+    "hash": "90a7fbb66ac57dc2bd69e50f563e9b09",
+    "name": "Frysk"
   },
   {
-    code: "fr",
-    hash: "7b39730f10fcbe5706da2ffebf2dbe2d",
-    name: "Français",
+    "code": "fr",
+    "hash": "95363391cc63ea2c68c0b03fe6d5cc4c",
+    "name": "Français"
   },
   {
-    code: "ga",
-    hash: "dc0aacba422388bb56194968beed5770",
-    name: "Gaeilge",
+    "code": "ga",
+    "hash": "d32f08eb4657a3588bd20d2a0819ab79",
+    "name": "Gaeilge"
   },
   {
-    code: "gl",
-    hash: "ab7175f61ce1bf376306ac5fd45b72f9",
-    name: "Galego",
+    "code": "gl",
+    "hash": "bf4605fe05892c177d203d2d9cb37b1c",
+    "name": "Galego"
   },
   {
-    code: "gsw",
-    hash: "7682849c5bf2d5bc38778d143fd276b2",
-    name: "Schwiizerdütsch",
+    "code": "gsw",
+    "hash": "5fc36b9fdf7f40767ea7367b000520c7",
+    "name": "Schwiizerdütsch"
   },
   {
-    code: "he",
-    hash: "8202ea5bfc2369dcaa66bf6105f3a60d",
-    name: "עברית",
+    "code": "he",
+    "hash": "d69665b1912bb299ea74571c0ecf5ab0",
+    "name": "עברית"
   },
   {
-    code: "hi",
-    hash: "311eff0e36fd5049dba35d916bdd1e98",
-    name: "हिन्दी",
+    "code": "hi",
+    "hash": "e890c283ad2548e2feb46ef66310d707",
+    "name": "हिन्दी"
   },
   {
-    code: "hr",
-    hash: "7e207e37b524e874d8e3e23a6cb71812",
-    name: "Hrvatski",
+    "code": "hr",
+    "hash": "e4f13c08ffb3f1cd856d11931ac3a6e8",
+    "name": "Hrvatski"
   },
   {
-    code: "hu",
-    hash: "bee45d457a97441e9a60fd9d9960c300",
-    name: "Magyar",
+    "code": "hu",
+    "hash": "02949e77c5cf0c6bb4e1b2fb3c2a0e41",
+    "name": "Magyar"
   },
   {
-    code: "hy",
-    hash: "87e65f6f4a7a1edfa251cfebda3797de",
-    name: "Հայերեն",
+    "code": "hy",
+    "hash": "578e656368d2fa61698cba3e42c647f6",
+    "name": "Հայերեն"
   },
   {
-    code: "id",
-    hash: "08060c936538d653e65a517eaf98be19",
-    name: "Indonesia",
+    "code": "id",
+    "hash": "4eec5b9abfdf0b8569ab5834e76ac35a",
+    "name": "Indonesia"
   },
   {
-    code: "it",
-    hash: "90a0d6efcc3c3274c6f0700befa25cde",
-    name: "Italiano",
+    "code": "it",
+    "hash": "06a2105d1cc1ffa252f8161de4a62f4b",
+    "name": "Italiano"
   },
   {
-    code: "is",
-    hash: "bc45a464b73488a263ab57f29f3e83c4",
-    name: "Íslenska",
+    "code": "is",
+    "hash": "a8d96e7d2714ed253eee1033572090d0",
+    "name": "Íslenska"
   },
   {
-    code: "ja",
-    hash: "f0a220270492199d710108c38529c4d8",
-    name: "日本語",
+    "code": "ja",
+    "hash": "1572ed7d47496968ffac1cbc3667ea48",
+    "name": "日本語"
   },
   {
-    code: "ka",
-    hash: "127c1a375debe953f6005f193ea4373b",
-    name: "Kartuli",
+    "code": "ka",
+    "hash": "12a74745e9196f7be89025b86946032b",
+    "name": "Kartuli"
   },
   {
-    code: "ko",
-    hash: "b3785eb77629bdf90526d590c63e6cfe",
-    name: "한국어",
+    "code": "ko",
+    "hash": "fd8925212bfb415e1fbe1e50cfac1a29",
+    "name": "한국어"
   },
   {
-    code: "lb",
-    hash: "296254c20a83d3abb86f65ec7594dfb7",
-    name: "Lëtzebuergesch",
+    "code": "lb",
+    "hash": "675aba3ddebd914a84cea7baa97db078",
+    "name": "Lëtzebuergesch"
   },
   {
-    code: "lt",
-    hash: "697ab244c646b0c9f616e8a12d7d6cd9",
-    name: "Lietuvių",
+    "code": "lt",
+    "hash": "489ab50daa5c400ab400ec7bcce74336",
+    "name": "Lietuvių"
   },
   {
-    code: "lv",
-    hash: "b407c3cd78720cf9845d06c423462583",
-    name: "Latviešu",
+    "code": "lv",
+    "hash": "40ed7fc5234225dca8a29d2c933ff208",
+    "name": "Latviešu"
   },
   {
-    code: "mk",
-    hash: "3ec7f7400a37dc258a9ea64b086648dd",
-    name: "Македонски",
+    "code": "mk",
+    "hash": "aba899589e4206f67dec627d5038bd9c",
+    "name": "Македонски"
   },
   {
-    code: "ml",
-    hash: "5c3f3ec3b93e4c03a45c9c8e44a4f3cc",
-    name: "മലയാളം",
+    "code": "ml",
+    "hash": "9572e055183df2f9d73a841ecce393fb",
+    "name": "മലയാളം"
   },
   {
-    code: "nl",
-    hash: "221cd249653a1e291a73f887d5945395",
-    name: "Nederlands",
+    "code": "nl",
+    "hash": "dd15f06f25625eb6c9d7f628d996d15a",
+    "name": "Nederlands"
   },
   {
-    code: "nb",
-    hash: "b0c47fc92c0a9173c73e2a962f28d8cb",
-    name: "Norsk Bokmål",
+    "code": "nb",
+    "hash": "9903a8bedd262429edde832c5523bd93",
+    "name": "Norsk Bokmål"
   },
   {
-    code: "nn",
-    hash: "3cac7c697b51da2529f8b733260c4716",
-    name: "Norsk Nynorsk",
+    "code": "nn",
+    "hash": "92df1f607b897a66838bfc39bbd21f65",
+    "name": "Norsk Nynorsk"
   },
   {
-    code: "pl",
-    hash: "bb84843cc40a0ac28a98c6d3f3f3fd10",
-    name: "Polski",
+    "code": "pl",
+    "hash": "d2ea83f477c567dfbfb63d14710ac4a4",
+    "name": "Polski"
   },
   {
-    code: "pt",
-    hash: "e2b04f3b031ae6364a4b04bd23bfd5cf",
-    name: "Português",
+    "code": "pt",
+    "hash": "9105a30f05dd48a7a7d4c210d0698069",
+    "name": "Português"
   },
   {
-    code: "pt-BR",
-    hash: "2f2e51cd7ed7515e1f5d15f313927a43",
-    name: "Português (BR)",
+    "code": "pt-BR",
+    "hash": "26071e74016ec3bacdc67a2e577b51ef",
+    "name": "Português (BR)"
   },
   {
-    code: "ro",
-    hash: "e818fbba08d6064e8aaf05c7726bc556",
-    name: "Română",
+    "code": "ro",
+    "hash": "2200d5c872a275bb7eb23654becd3d3e",
+    "name": "Română"
   },
   {
-    code: "ru",
-    hash: "0eb306736a628325ffd1d20657654c48",
-    name: "Русский",
+    "code": "ru",
+    "hash": "3e214a242f14fcf4a2879a286b91cb96",
+    "name": "Русский"
   },
   {
-    code: "sk",
-    hash: "06041317ae9fc90918f0b43fe1732c4e",
-    name: "Slovenčina",
+    "code": "sk",
+    "hash": "d79e6784e2435606346f6a07bfe15db0",
+    "name": "Slovenčina"
   },
   {
-    code: "sl",
-    hash: "34f5fd0a997d20113ec9276bc1f65653",
-    name: "Slovenščina",
+    "code": "sl",
+    "hash": "29147ed179686444e802174ab3d4c29c",
+    "name": "Slovenščina"
   },
   {
-    code: "sr",
-    hash: "fec5e416c2a603fa9336af91404b5b6b",
-    name: "Српски",
+    "code": "sr",
+    "hash": "665ae25ba1272d47209dbabacdd9ff05",
+    "name": "Српски"
   },
   {
-    code: "sr-Latn",
-    hash: "290a37d9ebee792d091003b7f2d8f1a7",
-    name: "Srpski",
+    "code": "sr-Latn",
+    "hash": "9b2f33cd34efb86610333ec20e5b43c8",
+    "name": "Srpski"
   },
   {
-    code: "sv",
-    hash: "2d1840c512399b8c219f36928d04bf6e",
-    name: "Svenska",
+    "code": "sv",
+    "hash": "69f974dbae40e7738fe7d79d01f6bc69",
+    "name": "Svenska"
   },
   {
-    code: "ta",
-    hash: "603014935b693388a425940bba4914b4",
-    name: "தமிழ்",
+    "code": "ta",
+    "hash": "501d996b6b465bb0085d66e07dbb67c3",
+    "name": "தமிழ்"
   },
   {
-    code: "te",
-    hash: "896d3aed07812883bdc1cd788beed61c",
-    name: "తెలుగు",
+    "code": "te",
+    "hash": "674830311ee6f631ed5806794499281d",
+    "name": "తెలుగు"
   },
   {
-    code: "th",
-    hash: "92407f0c797588e6fbc5fe1597bac438",
-    name: "ภาษาไทย",
+    "code": "th",
+    "hash": "c2f4aa565bb405e78585b1c528f46747",
+    "name": "ภาษาไทย"
   },
   {
-    code: "tr",
-    hash: "c24c8d3592a658ff2ceb6998906dd6ed",
-    name: "Türkçe",
+    "code": "tr",
+    "hash": "a2bc81bfc9903a8ab123a21bd6e832de",
+    "name": "Türkçe"
   },
   {
-    code: "uk",
-    hash: "276dc957e451d86ab734536233426744",
-    name: "Українська",
+    "code": "uk",
+    "hash": "83c6c51c53f5fc356e210753358b5ef7",
+    "name": "Українська"
   },
   {
-    code: "ur",
-    hash: "df3eea3c85a464a0191c6d5562ed07d5",
-    name: "اُردُو",
+    "code": "ur",
+    "hash": "b6758b8939fb591f687b3a27c9aa7de4",
+    "name": "اُردُو"
   },
   {
-    code: "vi",
-    hash: "89fabe26e58defb1f468d7994f628652",
-    name: "Tiếng Việt",
+    "code": "vi",
+    "hash": "f92ae01d331719c3454dfe167f760b95",
+    "name": "Tiếng Việt"
   },
   {
-    code: "zh-Hans",
-    hash: "954c76ed73614940446cf80ccb2c4026",
-    name: "简体中文",
+    "code": "zh-Hans",
+    "hash": "7c7b90e806c1b9d0454cd1ec770936be",
+    "name": "简体中文"
   },
   {
-    code: "zh-Hant",
-    hash: "f584bc01b5ceee538fc6e4997a93fe3f",
-    name: "繁體中文",
-  },
+    "code": "zh-Hant",
+    "hash": "c660de477e6dcadf75dec296244287c1",
+    "name": "繁體中文"
+  }
 ] satisfies Array<{
   code: Locales;
   hash: string;
   name: string;
 }>;
 
-export default locales.map((locale) => ({
+export default locales.map(locale => ({
   ...locale,
   async fetch(): Promise<LocaleData> {
     const cached = CACHE[locale.code];
-    if (typeof cached !== "undefined") return cached;
-    const data: {
-      default: LocaleData;
+    if (typeof cached !== 'undefined') return cached;
+    const data : {
+      default: LocaleData
     } = await import(`./${locale.code}/${locale.code}.json`);
     CACHE[locale.code] = data.default;
     return data.default;
-  },
+  }
 }));
