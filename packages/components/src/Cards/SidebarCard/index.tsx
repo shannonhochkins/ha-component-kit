165bd57106c6873d4306a2dbddf646287971841a
Danny Allen <me@dannya.com>
2026-02-11 23:47:22 +0000

    feat: `SidebarCard`: set `tooltipsWhenClosed` prop to `true` by default

--- packages/components/src/Cards/SidebarCard/index.tsx
+++ packages/components/src/Cards/SidebarCard/index.tsx
@@ -276,7 +276,7 @@ function InternalSidebarCard({
   },
   startOpen = true,
   collapsible = true,
-  tooltipsWhenClosed = false,
+  tooltipsWhenClosed = true,
   menuItems = [],
   children,
   autoIncludeRoutes = true,
